5ae9ccea107abf46143ac1ae73f613a7780f63db
Rooboy1818 <marcel.m@beigetech.com.au>
2024-06-21 15:49:48 +1000

    Update Vis.js

--- graph/Vis.js
+++ graph/Vis.js
@@ -154,35 +154,6 @@ function addnodes(
       resource: resourceObject,
       font: {
         size: 20
-      },
-      /*ctxRenderer: function({ ctx, x, y, state: { selected, hover }, style }) {
-        console.log('marcel ctxRenderer');
-        const r = style.size;
-        ctx.beginPath();
-        const sides = 6;
-        const a = (Math.PI * 2) / sides;
-        ctx.moveTo(x , y + r);
-        for (let i = 1; i < sides; i++) {
-            ctx.lineTo(x + r * Math.sin(a * i), y + r * Math.cos(a * i));
-        }
-        ctx.closePath();
-        ctx.save();
-        ctx.fillStyle = 'red';
-        ctx.fill(); 
-        ctx.stroke();
-        ctx.restore();
-    
-        ctx.font = "normal 12px sans-serif";
-        ctx.fillStyle = 'black';
-      }*/
-      ctxRenderer: function ctxRenderer(all) {
-        return {
-          drawNode() {
-            all.ctx.fillStyle = 'green'
-            all.ctx.fillRect(all.x, all.y, 100, 100)
-          },
-          nodeDimensions: { width: 100, height: 100 },
-        }
       }
     });
   }
