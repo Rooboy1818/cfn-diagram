54f30c84d44aaf0cd7a7de0ffeaa286fc379b56e
Rooboy1818 <marcel.m@beigetech.com.au>
2024-06-21 15:27:24 +1000

    Update Vis.js

--- graph/Vis.js
+++ graph/Vis.js
@@ -155,29 +155,27 @@ function addnodes(
       font: {
         size: 20
       },
-      ctxRenderer: drawNode({ ctx, x, y, state: { selected, hover }, style })
+      ctxRenderer: function({ ctx, x, y, state: { selected, hover }, style }) {
+        const r = style.size;
+        ctx.beginPath();
+        const sides = 6;
+        const a = (Math.PI * 2) / sides;
+        ctx.moveTo(x , y + r);
+        for (let i = 1; i < sides; i++) {
+            ctx.lineTo(x + r * Math.sin(a * i), y + r * Math.cos(a * i));
+        }
+        ctx.closePath();
+        ctx.save();
+        ctx.fillStyle = 'red';
+        ctx.fill(); 
+        ctx.stroke();
+        ctx.restore();
+    
+        ctx.font = "normal 12px sans-serif";
+        ctx.fillStyle = 'black';
+      }
     });
   }
-}
-
-function drawNode({ ctx, x, y, state: { selected, hover }, style }) {
-    const r = style.size;
-    ctx.beginPath();
-    const sides = 6;
-    const a = (Math.PI * 2) / sides;
-    ctx.moveTo(x , y + r);
-    for (let i = 1; i < sides; i++) {
-        ctx.lineTo(x + r * Math.sin(a * i), y + r * Math.cos(a * i));
-    }
-    ctx.closePath();
-    ctx.save();
-    ctx.fillStyle = 'red';
-    ctx.fill(); 
-    ctx.stroke();
-    ctx.restore();
-
-    ctx.font = "normal 12px sans-serif";
-    ctx.fillStyle = 'black';
 }
 
 function createImage(resourceType) {
