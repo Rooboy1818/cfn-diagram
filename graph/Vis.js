5088392d1247cdae959984ec3d0f910a6e435cde
Rooboy1818 <marcel.m@beigetech.com.au>
2024-06-21 15:24:02 +1000

    Update Vis.js

--- graph/Vis.js
+++ graph/Vis.js
@@ -116,10 +116,10 @@ function addEdges(from, to, dependencyNode, fromNode) {
             type: "circle"
           },
         },
-        smooth: {
+        //smooth: {
         //  type: "cubicBezier",
-          roundness: 1
-        }
+        //  roundness: 1
+        //}
       });
     }
   }
@@ -154,9 +154,30 @@ function addnodes(
       resource: resourceObject,
       font: {
         size: 20
-      }
+      },
+      ctxRenderer: drawMode({ ctx, x, y, state: { selected, hover }, style })
     });
   }
+}
+
+function drawNode({ ctx, x, y, state: { selected, hover }, style }) {
+    const r = style.size;
+    ctx.beginPath();
+    const sides = 6;
+    const a = (Math.PI * 2) / sides;
+    ctx.moveTo(x , y + r);
+    for (let i = 1; i < sides; i++) {
+        ctx.lineTo(x + r * Math.sin(a * i), y + r * Math.cos(a * i));
+    }
+    ctx.closePath();
+    ctx.save();
+    ctx.fillStyle = 'red';
+    ctx.fill(); 
+    ctx.stroke();
+    ctx.restore();
+
+    ctx.font = "normal 12px sans-serif";
+    ctx.fillStyle = 'black';
 }
 
 function createImage(resourceType) {
